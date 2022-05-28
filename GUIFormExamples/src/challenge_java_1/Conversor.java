7519c60e771be51b41650790db2b538ca366c859
Marcos-Castillo <93360137+Marcos-Castillo@users.noreply.github.com>
2022-05-27 22:41:45 -0300

    validaciones
    finalizando validaciones

--- GUIFormExamples/src/challenge_java_1/Conversor.java
+++ GUIFormExamples/src/challenge_java_1/Conversor.java
@@ -4,6 +4,31 @@ import javax.swing.JFrame;
 import javax.swing.JOptionPane;
 
 public class Conversor {
+        String conversores[] = {
+            "Conversor de Moneda",
+            "Conversor de temperatura"
+        };
+
+        String conversorMoneda[] = {
+            "la moneda de tu país  a Dólar",
+            "la moneda de tu país  a Euros",
+            "la moneda de tu país  a Libras Esterlinas",
+            "la moneda de tu país  a Yen Japonés",
+            "la moneda de tu país  a Won sul-coreano",
+            "Dólar a la moneda de tu país",
+            "Euros a la moneda de tu país",
+            "Libras Esterlinas a la moneda de tu país",
+            "Yen Japonés a la moneda de tu país",
+            "Won sul-coreano a la moneda de tu país"
+        };
+        String conversorTemperatura[] = {
+            "Grados Celcius a Grados Farenheit",
+            "Grados Celcius a Kelvin",
+            "Grados Farenheit a Grados Celcius",
+            "Kelvin a Grados Celcius",
+            "Kelvin a Grados Farenheit"
+        };
+    String prueba = conversorMoneda[0];
 
     public String selectInput(Object msg, String titulo, String descripcion) {
 
@@ -23,7 +48,7 @@ public class Conversor {
         return JOptionPane.showConfirmDialog(
                 null,
                 "¿Deseas continuar?",
-                "titulo ventana",
+                "Conversor",
                 JOptionPane.YES_NO_OPTION,
                 JOptionPane.WARNING_MESSAGE
         );
@@ -33,39 +58,54 @@ public class Conversor {
         switch(msg)
         {
     case "la moneda de tu país  a Dólar":
-        valor *= 1.5;
+        valor *= 0.0084;
           break; 
     case "la moneda de tu país  a Euros" :
-        valor *= 1.5;
+        valor *= 0.0078;
          break; 
     case "la moneda de tu país  a Libras Esterlinas":
-        valor *= 1.5;
+        valor *= 0.0066;
           break; 
     case "la moneda de tu país  a Yen Japonés" :
-        valor *= 1.5;
+        valor *= 1.06;
          break;             
     case "la moneda de tu país  a Won sul-coreano":
-        valor *= 1.5;
+        valor *= 10.53;
           break; 
     case "Dólar a la moneda de tu país" :
-        valor *= 1.5;
+        valor *= 119.48;
          break; 
     case "Euros a la moneda de tu país":
-        valor *= 1.5;
+        valor *= 128.23;
           break; 
     case "Libras Esterlinas a la moneda de tu país":
-        valor *= 1.5;
+        valor *= 150.90;
          break;  
     case "Yen Japonés a la moneda de tu país":
-        valor *= 1.5;
+        valor *= 0.94;
          break;  
     case "Won sul-coreano a la moneda de tu país":
-        valor *= 1.5;
+        valor *= 0.095;
          break;  
+    case "Grados Celcius a Grados Farenheit":
+        valor = (valor * 9/5) + 32 ;
+         break;  
+    case "Grados Celcius a Kelvin":
+        valor += 273.15 ;
+         break;       
+    case "Grados Farenheit a Grados Celcius":
+        valor = (valor - 32)*5/9;
+         break;  
+    case "Kelvin a Grados Celcius":
+        valor -=273.15 ;
+         break;       
+    case  "Kelvin a Grados Farenheit":
+        valor = (valor - 273.15) * 9/5 + 32 ;
+         break;  
+          
     default : 
        
    }
-
     return valor;
    }
 }
--- GUIFormExamples/src/challenge_java_1/TestConversor.java
+++ GUIFormExamples/src/challenge_java_1/TestConversor.java
@@ -6,25 +6,10 @@ import javax.swing.JOptionPane;
 public class TestConversor {
 
     public static void main(String[] args) {
-
-        String conversores[] = {
-            "Conversor de Moneda",
-            "Conversor de temperatura"
-        };
-
-        String conversoresMoneda[] = {
-            "la moneda de tu país  a Dólar",
-            "la moneda de tu país  a Euros",
-            "la moneda de tu país  a Libras Esterlinas",
-            "la moneda de tu país  a Yen Japonés",
-            "la moneda de tu país  a Won sul-coreano",
-            "Dólar a la moneda de tu país",
-            "Euros a la moneda de tu país",
-            "Libras Esterlinas a la moneda de tu país",
-            "Yen Japonés a la moneda de tu país",
-            "Won sul-coreano a la moneda de tu país"
-        };
-        Conversor c = new Conversor();
+        Conversor c = new Conversor();      
+        String conversores[] = c.conversores;
+        String conversorMoneda[] = c.conversorMoneda;
+        String conversorTemperatura[] = c.conversorTemperatura;
         int yn = 0;
     
        while(yn==0){
@@ -37,25 +22,45 @@ public class TestConversor {
 
         if (msg == conversores[0]) {
             msg = c.selectInput(
-                    conversoresMoneda,
+                    conversorMoneda,
                     "Conversor de Moneda",
                     "Selecciona la moneda en la que deseas convertir tu dinero"
             );
             double valor = 0;
-            valor = Double.parseDouble(JOptionPane.showInputDialog(null, "Introduce el monto a convertir"));
-            valor = c.conversor(valor, msg);
-
-            JOptionPane.showMessageDialog(null, "El monto final es de " + valor);
+            try{
+                valor += Double.parseDouble(JOptionPane.showInputDialog(null, "Introduce el monto a convertir","Conversor de Moneda",JOptionPane.QUESTION_MESSAGE));
+                valor = c.conversor(valor, msg);
+                JOptionPane.showMessageDialog(null, "El monto final es de " + valor,msg, 1);
+            }catch(Exception exe){
+                System.out.println("Excepcion "+exe);
+                JOptionPane.showMessageDialog(null,"El valor ingresado es invalido", "Error", JOptionPane.WARNING_MESSAGE);
+            }
+            
 
         } else if (msg == conversores[1]) {
-            System.out.println(msg + "2");
+             msg = c.selectInput(
+                    conversorTemperatura,
+                    "Conversor de temperatura",
+                    "Selecciona unidad de medida en la que deseas convertir"
+            );
+            double valor = 0;
+            try{
+            valor += Double.parseDouble(JOptionPane.showInputDialog(null, "Introduce la temperatura a convertir","Conversor de temperatura",JOptionPane.QUESTION_MESSAGE));
+            valor = c.conversor(valor, msg);
+            JOptionPane.showMessageDialog(null, "Temperatura final: " + valor,msg, 1);
+            
+            }catch(Exception exe){
+                System.out.println("Excepcion "+exe);
+                JOptionPane.showMessageDialog(null,"El valor ingresado es invalido", "Error", JOptionPane.WARNING_MESSAGE);
+            }
+
         }
 
         yn = c.ventanaYesNot();
           
         
        }
-    
+     System.exit(0);
 }
 }
 
